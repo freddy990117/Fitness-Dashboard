fca98e7794c60adfe07f8e42c53a8f1ba39385f6
Freddy990117 <Freddy990117@gmail.com>
2025-07-12 07:09:13 +0800

    Setup Router (Default Login)

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+      <h1>LOgin</h1>
+    </div>
+  );
+};
