4ebba09720d65cf5989e21f2eb758578872bff83
Freddy990117 <Freddy990117@gmail.com>
2025-07-12 09:04:46 +0800

    Set up Login page's structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,16 +2,27 @@ import React from "react";
 
 export const Login = () => {
   return (
-    <div>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
-      <h1>LOgin</h1>
+    <div className="login-wrapper">
+      <div className="login-page">
+        <div className="login-section">
+          <div className="login-page__form">
+            <form>
+              <input type="email" name="" id="accout" placeholder="Email...." />
+              <input
+                type="password"
+                name=""
+                id="password"
+                placeholder="Password...."
+              />
+              <input type="checkbox" /> Remeber me
+              <button type="submit">Sign In</button>
+            </form>
+          </div>
+          <div className="login-page__image">
+            <img src="/assets/Login.png" alt="Login" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
