2e98df1383df22ae61f6749507b0070e173b4c9a
Freddy990117 <Freddy990117@gmail.com>
2025-07-14 07:16:08 +0800

    Setup Sidebar's Layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar-wrapper">
+      <div className="sidebar-header">
+        <img src="/public/vite.svg" alt="user1_Photo" />
+        <h2>user1</h2>
+      </div>
+      <div className="sidebar-menu">
+        <h3>
+          <a href="#">Home</a>
+        </h3>
+        <h3 className="disable">
+          <a href="#">Profile (Under Development)</a>
+        </h3>
+        <h3 className="disable">
+          <a href="#">Calendar (Under Development)</a>
+        </h3>
+        <h3 className="disable">
+          <a href="#">Setting (Under Development)</a>
+        </h3>
+        <h3>
+          <a href="#">Layout (Under Development)</a>
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
