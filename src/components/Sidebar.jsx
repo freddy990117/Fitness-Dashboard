a1d740b4422268700e47fe48d9ba2e9a0308a552
Freddy990117 <Freddy990117@gmail.com>
2025-07-14 20:49:39 +0800

    Add font awesome cdn & icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import "../styles/sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faChildReaching,
+  faCalendarDays,
+  faGear,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 const Sidebar = () => {
   return (
     <div className="sidebar-wrapper">
       <div className="sidebar-header">
-        <img src="/public/vite.svg" alt="user1_Photo" />
+        <img src="/vite.svg" alt="user1_Photo" />
         <h2>user1</h2>
       </div>
       <div className="sidebar-menu">
         <h3>
-          <a href="#">Home</a>
+          <FontAwesomeIcon icon={faHouse} /> <a href="#">Home</a>
         </h3>
         <h3 className="disable">
+          <FontAwesomeIcon icon={faChildReaching} />{" "}
           <a href="#">Profile (Under Development)</a>
         </h3>
         <h3 className="disable">
+          <FontAwesomeIcon icon={faCalendarDays} />
           <a href="#">Calendar (Under Development)</a>
         </h3>
         <h3 className="disable">
+          <FontAwesomeIcon icon={faGear} />
           <a href="#">Setting (Under Development)</a>
         </h3>
         <h3>
+          <FontAwesomeIcon icon={faRightFromBracket} />
           <a href="#">Layout</a>
         </h3>
       </div>
