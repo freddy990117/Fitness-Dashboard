5fdd8f7b159c156f1b176a202cf60167bc7f3136
Freddy990117 <Freddy990117@gmail.com>
2025-07-20 06:37:32 +0800

    Add Card Body's Layout & CSS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,16 +18,32 @@ const Card = () => {
       </div>
       <div className="card-body">
         <div className="card-item card-weight">
-          <FontAwesomeIcon icon={faWeightScale} />
-          Weight
+          <div className="card-item-header">
+            <FontAwesomeIcon className="icon" icon={faWeightScale} />
+            <h1>Weight</h1>
+          </div>
+          <div className="card-item-body">
+            <h1>80kg</h1>
+          </div>
         </div>
         <div className="card-item card-workout">
-          <FontAwesomeIcon icon={faDumbbell} />
-          Workout
+          <div className="card-item-header">
+            <FontAwesomeIcon className="icon workout" icon={faDumbbell} />
+            <h1>Workout</h1>
+          </div>
+          <div className="card-item-body">
+            <h1> 1 times</h1>
+          </div>
         </div>
         <div className="card-item card-protein">
-          <FontAwesomeIcon icon={faEgg} />
-          Protein
+          <div className="card-item-header">
+            <FontAwesomeIcon className="icon" icon={faEgg} />
+            <h1>Protein</h1>
+          </div>
+          <div className="card-item-body">
+            <h1>80%</h1>
+            {/* Chart...... */}
+          </div>
         </div>
         <div className="card-bar card-weight-change">
           <h1>Weight Change Line Chart</h1>
@@ -35,13 +51,17 @@ const Card = () => {
           nam eius, numquam dolore omnis odit necessitatibus exercitationem
           dolorem, optio dolores ipsum ad animi ullam mollitia dolor illo
           obcaecati. Nulla!
+          {/* Chart......  */}
         </div>
         <div className="card-bar card-tip">
           <h1>Health Tip</h1>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-          placeat necessitatibus consequuntur quisquam neque dolorum explicabo
-          distinctio ab numquam a eos dignissimos quo atque tempora libero ut,
-          cum quam rerum.
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur placeat necessitatibus consequuntur quisquam neque
+            dolorum explicabo distinctio ab numquam a eos dignissimos quo atque
+            tempora libero ut, cum quam rerum.
+          </p>
         </div>
       </div>
     </section>
