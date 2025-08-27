013a963c9c11fd048496ea80a73e7ddc1fd07e88
Freddy990117 <Freddy990117@gmail.com>
2025-08-28 00:40:06 +0800

    Feat : Update to FireStore (user1)

--- src/pages/Setup.jsx
+++ src/pages/Setup.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "../styles/setup.css";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../services/firebase";
 
 const Setup = () => {
   // 用來紀錄 User 輸入的值
@@ -7,21 +9,11 @@ const Setup = () => {
   const [inputDate, setInputDate] = useState([]); // 日期
   const [inputWorkout, setInputWorkout] = useState(0); // 訓練次數
   const [inputProtein, setInputProtein] = useState(0); // 蛋白質攝取
-  // 取最後一個值出來給畫面呈現使用 (Weight)
-  const lastWeight =
-    inputWeight.length > 0 ? inputWeight[inputWeight.length - 1] : null;
-  // 上傳給 firebase 的值
-  const data = {
-    weight: lastWeight,
-    workoutCount: inputWorkout,
-    proteinPercent: inputProtein,
-  };
   // 確定輸入的值
   const [currentValue, setCurrentValue] = useState("");
   const [currentDate, setCurrentDate] = useState("");
   // !如果輸入框沒有值時觸發
   const [error, setError] = useState(false);
-  // 按下 button 會觸發的行為
   const [edit, setEdit] = useState(false); // 如果變成 true 代表全部編輯完成，會傳送到 fireStore 的狀態
   const [formStep, setFormStep] = useState(1); // 整體 form 的下一步狀態 （總共三步）
   const [weightStep, setWeightStep] = useState(1); // 體重的下一步狀態 （總共七步）
@@ -30,6 +22,36 @@ const Setup = () => {
   // 綁定按下 Enter 執行下一個表單的行為 (ref) + 當 formStep 改變時執行
   const workoutRef = useRef(null);
   const proteinRef = useRef(null);
+  // 取最後一個值出來給畫面呈現使用 (Weight)
+  const lastWeight =
+    inputWeight.length > 0 ? inputWeight[inputWeight.length - 1] : null;
+
+  // 這邊是給 Dashborad 畫面顯示的值
+  const showDataOnDashboard = {
+    weight: lastWeight,
+    workoutCount: inputWorkout,
+    proteinPercent: inputProtein,
+  };
+  // 這是要給 Chart.js 使用的資料（七天份）
+  const data = {
+    inputWeight,
+    inputDate,
+  };
+
+  // 當表單都輸入完畢後，將資料更新到 fireStore 上
+  const saveToFirestore = async (userid, showDataOnDashboard, data) => {
+    try {
+      // 存放使用者的資料，setDoc 覆蓋原本的值
+      await setDoc(doc(db, "users", userid), {
+        dashboard: showDataOnDashboard, // 給 Dashboard 顯示
+        history: data, // 上傳七天份的資料給 fireStore（後續給 Chart 引用）
+        createdAt: new Date().toISOString(),
+      });
+      console.log("Firestore 更新成功！");
+    } catch (error) {
+      console.error("上傳失敗", error);
+    }
+  };
 
   // 用於檢測按下鍵盤是否為 Enter 的行為
   const handleKeydown = (e) => {
@@ -77,9 +99,13 @@ const Setup = () => {
         return;
       }
       setError(false);
+      // 表單完成
       setEdit("Finish");
+      // 上傳資料到雲端上
+      saveToFirestore("user1", showDataOnDashboard, data);
     }
   };
+
   // 當表單發生改變時觸發
   useEffect(() => {
     if (formStep === 2 && workoutRef.current) {
