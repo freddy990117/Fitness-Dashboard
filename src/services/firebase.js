6afb50a5ada81140f857dff88ba4c8fec16fed70
Freddy990117 <Freddy990117@gmail.com>
2025-09-17 06:39:24 +0800

    Fix : fix Bad Request 400 for firebaseConfig,change use method (before  use .env)

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,16 +10,15 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDzJSa8KuBJSeGPQjex0NlYN3F0EFwgb1U",
+  authDomain: "fitness-dashboard-b1c44.firebaseapp.com",
+  projectId: "fitness-dashboard-b1c44",
+  storageBucket: "fitness-dashboard-b1c44.appspot.com",
+  messagingSenderId: "879370698697",
+  appId: "1:879370698697:web:64e65819b73c0e00d77218",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig); // 告訴 Firebase 你要連哪個專案（用 .env 提供的參數）
 export const auth = getAuth(app); // 拿到這個專案的「登入功能服務」物件
 export const db = getFirestore(app); // 拿到這個專案的「資料庫服務」物件
-
